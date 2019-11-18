421c37c5edcb28c8f717522029a34623f09c02f0
Yaxo-O <aaazzzx@mail.ru>
2019-11-18 10:25:03 +0000

    Notes tests

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,24 +1,58 @@
 import Note from '@entities/Note';
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import { logger } from '../logger';
 
 export default class NoteController {
 	public static async createNote(req: Request, res: Response) {
+		logger.info('Create note was called;');
+
+		if (!req.body.text || req.body.text && !req.body.text.trim()) {
+			res.status(400);
+
+			return;
+		}
+
 		const repository = getRepository(Note);
 		const note = new Note();
 		note.text = req.body.text;
+		note.dateCreated = new Date();
 
 		const result = await repository.save(note);
 		res.status(201).json(result);
 	}
 
 	public static async getNotes(req: Request, res: Response) {
+		logger.info('Get notes was requested;');
 		const repository = getRepository(Note);
 
 		res.status(200).send(await repository.find());
 	}
 
 	public static async getFilteredNotes(req: Request, res: Response) {
-		res.status(200).send('Get Filtered Notes');
+		logger.info('Get filtered notes was requested;');
+
+		const repository = getRepository(Note);
+		let query = repository.createQueryBuilder('note');
+
+		if (req.body.text) {
+			const text = req.body.text.toLowerCase();
+			logger.info(`Query text: ${text}`);
+			query = query.where(`lower(note.text) Like '%${text}%'`);
+		}
+
+		if (req.body.dateCreatedFrom && new Date(req.body.dateCreatedFrom)) {
+			const dateFrom = new Date(req.body.dateCreatedFrom);
+			logger.info(`Date created from: ${dateFrom}`);
+			query = query.andWhere('note.dateCreated >= :date', { date: dateFrom });
+		}
+
+		if (req.body.dateCreatedTo && new Date(req.body.dateCreatedTo)) {
+			const dateTo = new Date(req.body.dateCreatedTo);
+			logger.info(`Date created to: ${dateTo}`);
+			query = query.andWhere('note.dateCreated < :date', { date: dateTo });
+		}
+
+		res.status(200).json(await query.getMany());
 	}
 }
--- tests/noteController.test.ts
+++ tests/noteController.test.ts
@@ -3,12 +3,12 @@ import { Request, Response } from 'express';
 
 const notesList = [
 	{
-		dateCreated: new Date('23-02-1997'),
+		dateCreated: new Date('1997-02-23'),
 		id: 1,
 		text: 'First note',
 	},
 	{
-		dateCreated: new Date('24-11-1999'),
+		dateCreated: new Date('1999-11-24'),
 		id: 2,
 		text: 'Second note',
 	},
@@ -20,11 +20,16 @@ jest.mock('typeorm', () => ({
 	PrimaryGeneratedColumn: () => jest.fn(),
 	getRepository: jest.fn().mockReturnValue({
 		find: () => Promise.resolve(notesList),
+		save: () => Promise.resolve({
+			dateCreated: new Date('1963-11-23'),
+			id: 1,
+			text: 'Some note text',
+		}),
 	}),
 }));
 
 describe('Note Controller tests', () => {
-	it('Get Notes test (successful db connection)', async () => {
+	it('Get notes', async () => {
 		const request: Partial<Request> = {};
 		const response: Partial<Response> = {};
 		response.status = jest.fn().mockReturnValue(response);
@@ -34,5 +39,72 @@ describe('Note Controller tests', () => {
 
 		expect(response.status).toBeCalledWith(200);
 		expect(response.send).toBeCalledWith(notesList);
+	});
+
+	it('Create note: valid note', async () => {
+		const request: Partial<Request> = {
+			body: {
+				text: 'Some note text',
+			},
+		};
+		const response: Partial<Response> = {};
+		response.status = jest.fn().mockReturnValue(response);
+		response.json = jest.fn().mockReturnValue(response);
+
+		await NoteController.createNote(request as any, response as any);
+
+		expect(response.status).toBeCalledWith(201);
+		expect(response.json).toBeCalledWith({
+			dateCreated: new Date('1963-11-23'),
+			id: 1,
+			text: 'Some note text',
+		});
+	});
+
+	it('Create note: empty note', async () => {
+		const request: Partial<Request> = {
+			body: {
+				text: '',
+			},
+		};
+		const response: Partial<Response> = {};
+		response.status = jest.fn().mockReturnValue(response);
+		response.json = jest.fn().mockReturnValue(response);
+
+		await NoteController.createNote(request as any, response as any);
+
+		expect(response.status).toBeCalledWith(400);
+		expect(response.json).toBeCalledTimes(0);
+	});
+
+	it('Create note: whitespace note', async () => {
+		const request: Partial<Request> = {
+			body: {
+				text: '  ',
+			},
+		};
+		const response: Partial<Response> = {};
+		response.status = jest.fn().mockReturnValue(response);
+		response.json = jest.fn().mockReturnValue(response);
+
+		await NoteController.createNote(request as any, response as any);
+
+		expect(response.status).toBeCalledWith(400);
+		expect(response.json).toBeCalledTimes(0);
+	});
+
+	it('Get filtered notes: no filters', async () => {
+		const request: Partial<Request> = {
+			body: {
+			},
+		};
+		const response: Partial<Response> = {};
+		response.status = jest.fn().mockReturnValue(response);
+		response.json = jest.fn().mockReturnValue(response);
+
+		await NoteController.getFilteredNotes(request as any, response as any);
+
+		expect(response.status).toBeCalledWith(200);
+		expect(response.json).toBeCalledWith(notesList);
 	});
 });
